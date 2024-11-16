cd07044c839f5149537fe11c6161b7bd68944b7c
Hugo Reis <hbssreis@gmail.com>
2024-11-16 18:16:35 +0000

    feat: implement user card for logged user

--- components/UserCard.js
+++ components/UserCard.js
@@ -1,19 +1,34 @@
+"use client";
+
+import { signOut } from "next-auth/react";
 import Image from "next/image";
 
 export default function UserCard({ user }) {
-  const greeting = user?.name ? `Hello, ${user.name}!` : null;
+  const greeting = user?.name ? `${user.name}` : null;
 
-  const emailDisplay = user?.email ? `Email: ${user.email}!` : null;
+  const emailDisplay = user?.email ? `${user.email}` : null;
 
   const userImage = user?.image ? (
-    <Image src={user?.image} alt={user?.name ?? "Profile Pic"} width={200} height={200} priority={true} />
+    <Image
+      src={user?.image}
+      className="rounded-full"
+      alt={user?.name ?? "Profile Pic"}
+      width={50}
+      height={50}
+      priority={true}
+    />
   ) : null;
 
   return (
-    <section className="border-2 border-red">
-      {greeting}
-      {emailDisplay}
+    <div className="flex items-center space-x-4 border-red w-fit">
+      <div>
+        <p className="text-white">{greeting}</p>
+        <p className="text-white">{emailDisplay}</p>
+        <button onClick={() => signOut()} className="text-red-600 transition-colors hover:text-red-700">
+          Logout
+        </button>
+      </div>
       {userImage}
-    </section>
+    </div>
   );
 }
